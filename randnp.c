0c0d8d08a37612f50b16f293c3dc8e4011f837df
Unee0x <Unee0x@gmail.com>
2024-01-14 22:51:58 -0500

    commits

--- randnp.c
+++ randnp.c
@@ -5,13 +5,7 @@
 #define MAXLEN 9
 
 int getrand(){
-
-  int i = 0;
-
-  int buf[MAXLEN] = {'\0'};
-  int r = rand()%8 + 0;
-
-  printf("r is %d\n", r);
+  int r = rand()%9 + 1;
   return r;
 }
 
@@ -19,16 +13,25 @@ int main(){
   srand(time(NULL));
   int array[MAXLEN];
   int i = 0;
-  static int j = 0;
+  int j = 0;
+  int z = 0;
+  //int t = 0;
   while(i < MAXLEN){
     array[i] = getrand();
-
-    for(j = 0; j < MAXLEN;j++){
-      if(array[i] == array[j])
-	printf("Match at i = %d and j = %d with #. %d\n", i,j, array[i]);
-      
+    puts("first randomize");
+    array[z] = array[i];
+    //    z++;
+    printf("array[%d] is now %d\n", z, array[z]); 
+    for(j = 0;j < i;j++){
+      while(array[j] == array[i]){
+	printf("\tMatch at i = %d and j = %d with #. %d\n", i,j, array[i]);
+	puts("second randomize");
+	array[i] = getrand();
+	printf("\tarray[%d] is now %d\n", i, array[i]);
+      }
+      z++;
     }
-    printf("At index %d is %d\n", i, array[i]);
+    //printf("array[%d] is now %d\n", i, array[i]);
     i++;
     } 
   return 0;
